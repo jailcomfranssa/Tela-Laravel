6f0acfd143c12ae05a55272305628691f9bfcbf7
jailcomfranssa <you@example.com>
2019-08-01 13:10:13 -0300

    tela-inicio

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+
+
+   
+@section('content')
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet"> 
+<div class="container">
+    <div class="d-flex justify-content-center h-100">
+        
+            <div class="card">
+                <div class="card-header" style="color: white;font-size: 29px;">{{ __('Entra') }}</div>
+                
+                <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+				
+                                <div class="input-group form-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                        
+                                    </div>
+
+                                    <input id="email" type="email" placeholder="E-MAIL" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                                    
+                                </div>
+
+
+
+                                <div class="input-group form-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    
+                                    <input id="password" type="password" placeholder="SENHA" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                                </div>
+
+                               
+
+                               
+
+
+
+
+                                
+
+                                
+
+
+
+                                <div class="row align-items-center remember">
+                                            <div>
+                                                <input  type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <label  for="remember">
+                                                    {{ __('Lembre de mim') }}
+                                                </label>
+                                            </div>  
+                                </div>
+
+
+
+
+
+
+
+
+
+                                <div class="form-group ">
+                                        <div >
+                                            
+                                            <input type="submit" value="{{('Login')}}" class="btn float-right login_btn">
+                    
+                                            @if (Route::has('password.request'))
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </div>
+
+
+                        </form>
+                </div>
+            </div>
+        
+    </div>
+</div>
+@endsection
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Tangerine&display=swap" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Playball&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/inicio.css')}}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            
+            
+           
+
+           
+            
+           
+        </style>
+    </head>
+    @extends('layouts.app')
+    <body>
+   
+            
+            
+       <div class="container-fluid">
+       
+            <video id="vd" src="{{asset('video/videorest.mp4')}}" autoplay="true" loop="true"></video>
+
+            
+       </div>
+
+
+       <div class="container"  style="position: absolute;
+       margin-top: 129px;">
+            
+                    
+                    <div class="col-8">
+                            <div class="absolute">
+                                <a href="http://" id="logo"> Restaurante </a>
+                                <a href="http://"id="bem-vindo">Bem Vindo</a>
+                             
+                             
+                            </div>
+                             
+                            <div id="fundo-titulo"></div>
+                        
+                    </div>
+                    
+                
+       </div>
+
+       
+      
+        
+      
+    </body>
+</html>
+
